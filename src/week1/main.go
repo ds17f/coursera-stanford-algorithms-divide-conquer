2f05d050c9916d1e40601690167897b1b43c3da0
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-11-20 07:17:26 -0700

    week1: add some more tests, make it work with external tests, makefile
    * Add some more tests to the suite to make sure that things work
    * Add a makefile to build/run/test
    * Setup the main to take test cases from file input

--- src/week1/main.go
+++ src/week1/main.go
@@ -1,53 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
-	"github.com/damiansilbergleithcunniff/algorithms-divide-conquer/src/week1/strmath"
+	"github.com/damiansilbergleithcunniff/algorithms-divide-conquer/src/week1/algorithms"
 )
 
-func additionTest() {
-	fmt.Println(strmath.StrAdd("1234", "4567"))
+func readFile(fileName string) []string {
+	var fileLines []string
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-	fmt.Println(strmath.StrAdd("1234", "5678"))
-	fmt.Println(strmath.StrAdd("1234", "-5678"))
-	fmt.Println(strmath.StrAdd("-1234", "5678"))
-	fmt.Println(strmath.StrAdd("-1234", "-5678"))
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return fileLines
 }
+
 func main() {
-	additionTest()
-	//fmt.Println(recursiveIntegerMutliplication(1234, 5678))
+	if len(os.Args) != 2 {
+		binName := os.Args[0]
+		lastSlash := strings.LastIndex(binName, "/")
+		binName = binName[lastSlash+1 : len(binName)]
+		log.Fatalf("usage: %s <testFilename>", binName)
+	}
 
-	//fmt.Println(karatsuba(1234, 5678))
-	//fmt.Println(karatsubaString("1234", "5678"))
+	fileLines := readFile(os.Args[1])
+	if len(fileLines) != 2 {
+		log.Fatalf("error: test case must have 2 lines.  Had %d lines", len(fileLines))
+	}
 
-	//fmt.Println(karatsubaString("3141592653589793238462643383279502884197169399375105820974944592", "2718281828459045235360287471352662497757247093699959574966967627"))
-
-	//fmt.Println(karatsuba(1234, 5678))
-	//fmt.Println(karatsuba(123, 345))
-	//fmt.Println(karatsuba(4444, 2222))
-	//fmt.Println(leftPadPowOfTwo("100"))
-	//fmt.Println(leftPadPowOfTwo("109391"))
-	//fmt.Println(splitString("1234567890"))
-	//fmt.Println(strAdd("2", "3"))
-
-	//fmt.Println(strAdd("1234", "5678"))
-	//fmt.Println(strAdd("-1234", "5678"))
-	//fmt.Println(strAdd("1234", "-5678"))
-	//fmt.Println(strAdd("1934", "-5678"))
-	//fmt.Println(strAdd("-1934", "5678"))
-	// fmt.Println(strAdd(
-	// 	"3141592653589793238462643383279502884197169399375105820974944592",
-	// 	"2718281828459045235360287471352662497757247093699959574966967627"))
-	//fmt.Println(_strSub("100", "020"))
-	//fmt.Println(_strSub("12345", "06789"))
-
-	//fmt.Println(strSub("-1234", "5678"))
-	//fmt.Println(strSub("-1234", "-5678"))
-	//fmt.Println(strSub("1234", "-5678"))
-	//fmt.Println(strSub("1234", "5678"))
-	//println(strSub("703", "0684"))
-	//println(strSub("0019", "1710"))
-
-	//println(strAdd("56", "78"))
+	fmt.Println(algorithms.KaratsubaMult(fileLines[0], fileLines[1]))
 }
