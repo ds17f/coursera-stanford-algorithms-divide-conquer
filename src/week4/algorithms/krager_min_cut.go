02b3727596cd9b960bd78758691dc8f83e55c6d2
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-12-20 12:40:11 -0700

    week4: implement first stage, replace references to node

--- src/week4/algorithms/krager_min_cut.go
+++ src/week4/algorithms/krager_min_cut.go
@@ -1,5 +1,7 @@
 package algorithms
 
+import "strconv"
+
 // KragerMinCut implements Krager's randomized algorithm
 // for finding the minimum number of cuts in a graph.
 // input should be an adjacency list and the element at
@@ -7,4 +9,30 @@ package algorithms
 // entry in the array is the 11th numbered vertex
 func KragerMinCut(input [][]string) int {
 	return 0
+}
+
+// ReplaceReference replaces all the references to the node `from`
+// with the node `to` in the input adjacency list
+func ReplaceReference(input [][]string, from string, to string) {
+	fromI, _ := strconv.Atoi(from)
+	// This is the node that we are collapsing
+	// so we need to iterate through it and then
+	// replace all references to it with the node we're
+	// collapsing into
+	nodeToChange := input[fromI-1]
+	for _, v := range nodeToChange {
+		// each edge that the node touches needs to be examined
+		vI, err := strconv.Atoi(v)
+		if err == nil {
+			adjacentNode := input[vI-1]
+			// now iterate through the adjacentNode and replace
+			// the collapsed node with the node we collapse into.
+			for i, w := range adjacentNode {
+				if w == from {
+					adjacentNode[i] = to
+				}
+			}
+
+		}
+	}
 }
--- src/week4/algorithms/krager_min_cut_test.go
+++ src/week4/algorithms/krager_min_cut_test.go
@@ -1,6 +1,35 @@
 package algorithms_test
 
-import "testing"
+import (
+	"strings"
+	"testing"
+
+	"github.com/damiansilbergleithcunniff/algorithms-divide-conquer/src/week4/algorithms"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestKragerMinCut(t *testing.T) {
+}
+
+func TestReplaceReference(t *testing.T) {
+	testCase := [][]string{
+		{"2", "5"},
+		{"1", "3", "4"},
+		{"2", "4"},
+		{"2", "3", "5"},
+		{"1", "4"},
+	}
+	expectedResult := [][]string{
+		{"1", "5"},
+		{"1", "3", "4"},
+		{"1", "4"},
+		{"1", "3", "5"},
+		{"1", "4"},
+	}
+
+	algorithms.ReplaceReference(testCase, "2", "1")
+
+	for i := range testCase {
+		assert.Equal(t, strings.Join(expectedResult[i], ","), strings.Join(testCase[i], ","))
+	}
 }
